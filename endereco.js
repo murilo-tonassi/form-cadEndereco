d4ca23f8581d1f92f1d097693fcaf893cfcbf2f9
murilo-tonassi <murilo.tonassi@alunoescolassociais.org.br>
2024-09-20 07:30:42 -0300

    coloca o valor da API dentro do campo de formulário

--- endereco.js
+++ endereco.js
@@ -17,6 +17,14 @@ const limparFormulario = () =>{
 }
  
 // Verifica se o CEP é válido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const eNumero = (numero) => /^[0-9]+S/.test(numero);
 // Verifica o tamanho do CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('Rua').value = endereco.logradouro; // coloca o valor de logradouro de API dentro do campo logradouro do formulário
+    document.getElementById('Bairro').value = endereco.logradouro;
+    document.getElementById('Cidade').value = endereco.Cidade;
+    document.getElementById('uf').value = endereco.uf;
+}
